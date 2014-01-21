f06276e93b8b3c92544bd41057d20881706d1add
Konwisser <gekonwi@gmail.com>
2014-01-21 13:23:47 -0500

    finished implementing assignment requirements and unit tests

--- movie_data.rb
+++ movie_data.rb
@@ -1,13 +1,25 @@
 class User
-	attr_reader :id, :ratings
+	attr_reader :id
 
 	def initialize(user_id)
 		@id = user_id
-		@ratings = []
+		@movie_to_rating_num = {}
 	end
 
 	def add_rating(rating_obj)
-		@ratings << rating_obj
+		@movie_to_rating_num[rating_obj.movie_obj] = rating_obj.rating_num
+	end
+
+	def rating_num(movie_obj)
+		@movie_to_rating_num[movie_obj]
+	end
+
+	def rated_movies
+		@movie_to_rating_num.keys
+	end
+
+	def to_string
+		"User (id = #{@id}, ratings_count: #{@movie_to_rating_num.keys.length})"
 	end
 end
 
@@ -17,7 +29,7 @@ class Movie
 	SECONDS_IN_YEAR = 3600.0 * 24.0 * 365.25
 	private_constant :POPUL_HALF_LIFE_YEARS, :POPUL_EXP_BASE, :SECONDS_IN_YEAR
 
-	attr_reader :id, :ratings, :popularity
+	attr_reader :id, :popularity
 
 	def initialize(movie_id)
 		@id = movie_id
@@ -61,18 +73,100 @@ end
 
 class MovieData
 
-	U_DATA_FILE_PATH = "ml-100k/u.data"
+	MOST_SIMILAR_TOP_N = 10
 
 	def initialize
 		@id_to_movie = {}
 		@id_to_user = {}
-		@popularity_max_to_min_array = []
+		@popularity_desc_list = []
 	end
 
-	def load_data
-		f = File.open(U_DATA_FILE_PATH)
+	def load_data(file_path)
+		line_count = 0
+		File.readlines(file_path).each do |line|
+			read_line(line)
+			line_count += 1
+		end
+
+		@popularity_desc_list = @id_to_movie.values.sort {|a, b| b.popularity <=> a.popularity}
+
+		@max_user_distance = ((4 ** 2) * all_movies.length) ** 0.5
+
+		line_count
+	end
+
+	def all_users
+		@id_to_user.values
+	end
+
+	def all_movies
+		@id_to_movie.values
+	end
+
+	def user(user_id)
+		@id_to_user[user_id]
+	end
+
+	def movie(movie_id)
+		@id_to_movie[movie_id]
+	end
+
+	def popularity(movie_id)
+		movie(movie_id).popularity
+	end
+
+	def popularity_list
+		# returns copy in order to avoid changes of this array
+		Array.new(@popularity_desc_list)
+	end
+
+	def similarity(user1_id, user2_id)
+		# With n movies in total regard each user as a point in n dimensional space. The user's 
+		# rating for a particular movie is the coordinate in the corresponding dimension. If the 
+		# user did not rate the movie at all, the algorithm assumes the neutral rating '3'.
+		# The distance between the two points resulting from two users is inverse proportional to
+		# the similarity of these two users.
 		
-		while (line = f.gets)
+		user1, user2 = user(user1_id), user(user2_id)
+		# puts "", "calculating similarity between #{user1.to_string} and #{user2.to_string}"
+
+		square_sum = 0.0
+		
+		# performance tweak: iterating over the union of movies rated by one of the compared users
+		# (instead of iterating over all movies). Mathematically the movies which have not been
+		# rated by any of the two users would contribute the coordinate '3' for both and thus 0
+		# for the distance between the two resulting points. Therefore these movies can be skipped.
+		movies_union = user1.rated_movies | user2.rated_movies
+		movies_union.each do |movie|
+			rating1 = user1.rating_num(movie) || 3.0
+			rating2 = user2.rating_num(movie) || 3.0
+			
+			square_sum += (rating2 - rating1) ** 2.0
+		end
+
+		distance = square_sum ** 0.5
+		sim = 1.0 - distance / @max_user_distance
+		# puts "similarity: #{sim}"
+		sim
+	end
+
+	def most_similar(user_id)
+		# sort descending (biggest number -> most similar)
+		sorted_users = all_users.sort {|a, b| similarity(user_id, b.id) <=> similarity(user_id, a.id)}
+		result = []
+
+		# sorted_users[0].id == user_id, starting with index == 1
+		1.upto(MOST_SIMILAR_TOP_N) do |index|
+			break if index + 1 > sorted_users.length
+			result << sorted_users[index]
+		end
+
+		result
+	end
+
+	private
+
+		def read_line(line)
 			user_id_str, movie_id_str, rating_num_str, timestamp_num_str = line.split("\t")
 			
 			user_obj = get_or_create_user(user_id_str.to_i)
@@ -83,48 +177,13 @@ class MovieData
 			movie_obj.add_rating(rating_obj)
 		end
 
-		@popularity_max_to_min_array = @id_to_movie.values.sort do |a, b| 
-			if a.popularity > b.popularity
-				-1 # a follows b
-			elsif a.popularity == b.popularity
-				0 # a and b are equal
-			else
-				1 # b follows a
-			end
-		end
-	end
-
-	def popularity(movie_id)
-		movie(movie_id).popularity
-	end
-
-	def movie(movie_id)
-		@id_to_movie[movie_id]
-	end
-
-	def popularity_list
-		# returns copy in order to avoid changes of this array
-		Array.new(@popularity_max_to_min_array)
-	end
-
-	private
-
 		def get_or_create_user(user_id)
-			user = @id_to_user[user_id]
-			if user == nil
-				user = User.new(user_id)
-				@id_to_user[user_id] = user
-			end
-			user
+			user = @id_to_user[user_id] || User.new(user_id)
+			@id_to_user[user_id] = user
 		end
 
 		def get_or_create_movie(movie_id)
-			movie = @id_to_movie[movie_id]
-			if movie == nil
-				movie = Movie.new(movie_id)
-				@id_to_movie[movie_id] = movie
-			end
-			movie
+			movie = @id_to_movie[movie_id] || Movie.new(movie_id)
+			@id_to_movie[movie_id] = movie
 		end
-
 end
--- movie_data_test.rb
+++ movie_data_test.rb
@@ -1,83 +1,133 @@
 require_relative 'movie_data'
+require "test/unit"
 
-class PopularityTester
+# class PopularityTester
 
-	#MovieData.new.load_data
-	SECONDS_IN_YEAR = 3600.0 * 24.0 * 365.25
+# 	SECONDS_IN_YEAR = 3600.0 * 24.0 * 365.25
 
-	def run_test_dummy_popularity
-		puts "", "-------------------------------"
-		puts "run_test_dummy_popularity"
-		puts "-------------------------------", ""
+# 	def run_test_dummy_popularity
+# 		puts "", "-------------------------------"
+# 		puts "run_test_dummy_popularity"
+# 		puts "-------------------------------", ""
 
-		test_dummy_popularity(1, 0)
-		test_dummy_popularity(2, 1)
-		test_dummy_popularity(3, 4)
-		test_dummy_popularity(4, 5)
-		test_dummy_popularity(5, 6)
-		test_dummy_popularity(6, 10)
-		test_dummy_popularity(7, 100)
+# 		test_dummy_popularity(1, 0)
+# 		test_dummy_popularity(2, 1)
+# 		test_dummy_popularity(3, 4)
+# 		test_dummy_popularity(4, 5)
+# 		test_dummy_popularity(5, 6)
+# 		test_dummy_popularity(6, 10)
+# 		test_dummy_popularity(7, 100)
+# 	end
+
+# 	def run_test_read_popularity
+# 		puts "", "-------------------------------"
+# 		puts "run_test_read_popularity"
+# 		puts "-------------------------------", ""
+
+# 		movie_data = MovieData.new
+# 		movie_data.load_data
+
+# 		test_read_popularity(movie_data, 242)
+# 		test_read_popularity(movie_data, 302)
+# 		test_read_popularity(movie_data, 377)
+# 		test_read_popularity(movie_data, 51)
+# 		test_read_popularity(movie_data, 346)
+# 		test_read_popularity(movie_data, 474)
+# 		test_read_popularity(movie_data, 265)
+# 		test_read_popularity(movie_data, 465)
+# 		test_read_popularity(movie_data, 451)
+# 		test_read_popularity(movie_data, 86)
+# 	end
+
+# 	def run_all
+# 		#run_test_dummy_popularity
+# 		#run_test_read_popularity
+# 		print_popularity_list
+# 	end
+
+# 	private
+
+# 		def test_dummy_popularity(movie_id, before_years)
+# 			timestamp_num = Time.now.to_i - (before_years * SECONDS_IN_YEAR)
+# 			rating_obj = Rating.new(nil, nil, 0, timestamp_num)
+# 			movie_obj = Movie.new(movie_id)
+# 			movie_obj.add_rating(rating_obj)
+
+# 			puts "movie_id = #{movie_obj.id}"
+# 			puts "before_years = #{before_years}, timestamp = #{rating_obj.timestamp}"
+# 			puts "popularity = #{movie_obj.popularity}", ""
+# 		end
+
+# 		def test_read_popularity(movie_data, movie_id)
+# 			puts movie_data.movie(movie_id).to_string
+# 			puts "popularity = #{movie_data.popularity(movie_id)}", ""
+# 		end
+# end
+
+class TestMovieData < Test::Unit::TestCase
+	
+	U_DATA_FILE_PATH = "ml-100k/u.data"
+	U_TEST_DATA_FILE_PATH = "u_test.data"
+
+
+	def print_header(header)
+		puts "", ""
+		puts "----------------------------------------------"
+		puts header
+		puts "----------------------------------------------"
+		puts ""
 	end
 
-	def run_test_read_popularity
-		puts "", "-------------------------------"
-		puts "run_test_read_popularity"
-		puts "-------------------------------", ""
-
+	def test_read_test_data
 		movie_data = MovieData.new
-		movie_data.load_data
-
-		test_read_popularity(movie_data, 242)
-		test_read_popularity(movie_data, 302)
-		test_read_popularity(movie_data, 377)
-		test_read_popularity(movie_data, 51)
-		test_read_popularity(movie_data, 346)
-		test_read_popularity(movie_data, 474)
-		test_read_popularity(movie_data, 265)
-		test_read_popularity(movie_data, 465)
-		test_read_popularity(movie_data, 451)
-		test_read_popularity(movie_data, 86)
+		assert_equal(87, movie_data.load_data(U_TEST_DATA_FILE_PATH))
+		assert_equal(30, movie_data.all_users.length)
+		assert_equal(3, movie_data.all_movies.length)
 	end
 
-	def print_popularity_list
-		puts "", "-------------------------------"
-		puts "print_popularity_list"
-		puts "-------------------------------", ""
+	def print_list_to_string(list, from_index, to_index)
+		puts ""
+		from_index.upto(to_index) {|index| puts "#{index + 1}.: #{list[index].to_string}"}
+		puts ""
+	end
+
+	def test_print_real_popularity_list
+		print_header("test_print_real_popularity_list")
 
 		movie_data = MovieData.new
-		movie_data.load_data
+		movie_data.load_data(U_DATA_FILE_PATH)
 
 		list = movie_data.popularity_list
-		0.upto(9) {|index| puts "#{index + 1}.: #{list[index].to_string}"}
+		print_list_to_string(list, 0, 9)
 		puts "..."
-		
-		fromIndex, toIndex = list.length - 10, list.length - 1
-		fromIndex.upto(toIndex) {|index| puts "#{index + 1}.: #{list[index].to_string}"}
+		print_list_to_string(list, list.length - 10, list.length - 1)
 	end
 
-	def run_all
-		#run_test_dummy_popularity
-		#run_test_read_popularity
-		print_popularity_list
+	def test_user_to_user_similarity
+		movie_data = MovieData.new
+		movie_data.load_data(U_TEST_DATA_FILE_PATH)
+
+		max_user_distance = ((4 ** 2) * movie_data.all_movies.length) ** 0.5
+
+		assert_equal(movie_data.similarity(1, 2), movie_data.similarity(2, 1))
+		assert_in_delta(1.0 - 2.0 / max_user_distance, movie_data.similarity(1, 2), 0.0001)
 	end
 
-	private
+	def test_most_similar_on_test_data
+		movie_data = MovieData.new
+		movie_data.load_data(U_TEST_DATA_FILE_PATH)
 
-		def test_dummy_popularity(movie_id, before_years)
-			timestamp_num = Time.now.to_i - (before_years * SECONDS_IN_YEAR)
-			rating_obj = Rating.new(nil, nil, 0, timestamp_num)
-			movie_obj = Movie.new(movie_id)
-			movie_obj.add_rating(rating_obj)
-
-			puts "movie_id = #{movie_obj.id}"
-			puts "before_years = #{before_years}, timestamp = #{rating_obj.timestamp}"
-			puts "popularity = #{movie_obj.popularity}", ""
+		movie_data.most_similar(1).each do |user_obj| 
+			assert(user_obj.id > 10, "user_id #{user_obj.id} is < 11, shouldn't be in list")
+			assert(user_obj.id < 21, "user_id #{user_obj.id} is > 20, shouldn't be in list")
 		end
+	end
 
-		def test_read_popularity(movie_data, movie_id)
-			puts movie_data.movie(movie_id).to_string
-			puts "popularity = #{movie_data.popularity(movie_id)}", ""
-		end
+	def test_most_similar_on_real_data
+		movie_data = MovieData.new
+		movie_data.load_data(U_DATA_FILE_PATH)
+
+		list = movie_data.most_similar(1)
+		print_list_to_string(list, 0, list.length - 1)
+	end
 end
-
-PopularityTester.new.run_all
