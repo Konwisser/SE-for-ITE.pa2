77d6677dc6b8cbf50ad511c629a8bb81eab9d24d
Konwisser <gekonwi@gmail.com>
2014-01-21 00:42:28 -0500

    implemented popularity(movie_id) and popularity_list with corresponding tests

--- movie_data.rb
+++ movie_data.rb
@@ -12,7 +12,7 @@ class User
 end
 
 class Movie
-	POPUL_HALF_LIFE_YEARS = 5.0
+	POPUL_HALF_LIFE_YEARS = 3.0
 	POPUL_EXP_BASE = 0.5 ** (1.0 / POPUL_HALF_LIFE_YEARS)
 	SECONDS_IN_YEAR = 3600.0 * 24.0 * 365.25
 	private_constant :POPUL_HALF_LIFE_YEARS, :POPUL_EXP_BASE, :SECONDS_IN_YEAR
@@ -30,7 +30,15 @@ class Movie
 		@popularity += calc_popularity_addition(rating_obj)
 	end
 
+	def to_string
+		"Movie (id = #{@id}, ratings_count = #{@ratings.length}, popularity = #{@popularity})"
+	end
+
 	private
+		# A today's rating would the macimum to the film's popularity, which is 1.
+		# A rating which is POPUL_HALF_LIFE_YEARS old would add half of it, means 0.5.
+		# With t as time in years, the popularity addition P is
+		# P(t) = 1 * POPUL_EXP_BASE ^ t
 		def calc_popularity_addition(rating_obj)
 			now_seconds = Time.now.to_i
 			rating_seconds = rating_obj.timestamp.to_i
@@ -55,22 +63,68 @@ class MovieData
 
 	U_DATA_FILE_PATH = "ml-100k/u.data"
 
+	def initialize
+		@id_to_movie = {}
+		@id_to_user = {}
+		@popularity_max_to_min_array = []
+	end
+
 	def load_data
 		f = File.open(U_DATA_FILE_PATH)
 		
 		while (line = f.gets)
-			user_id, movie_id, rating_num, timestamp_num = line.split("\t")
+			user_id_str, movie_id_str, rating_num_str, timestamp_num_str = line.split("\t")
 			
-			user_obj = User.new(user_id)
-			movie_obj = Movie.new(movie_id)
+			user_obj = get_or_create_user(user_id_str.to_i)
+			movie_obj = get_or_create_movie(movie_id_str.to_i)
 
-			rating_obj = Rating.new(user_obj, movie_obj, rating_num, timestamp_num)
+			rating_obj = Rating.new(user_obj, movie_obj, rating_num_str.to_i, timestamp_num_str.to_i)
 			user_obj.add_rating(rating_obj)
 			movie_obj.add_rating(rating_obj)
+		end
+
+		@popularity_max_to_min_array = @id_to_movie.values.sort do |a, b| 
+			if a.popularity > b.popularity
+				-1 # a follows b
+			elsif a.popularity == b.popularity
+				0 # a and b are equal
+			else
+				1 # b follows a
+			end
 		end
 	end
 
 	def popularity(movie_id)
-		# TODO
+		movie(movie_id).popularity
 	end
+
+	def movie(movie_id)
+		@id_to_movie[movie_id]
+	end
+
+	def popularity_list
+		# returns copy in order to avoid changes of this array
+		Array.new(@popularity_max_to_min_array)
+	end
+
+	private
+
+		def get_or_create_user(user_id)
+			user = @id_to_user[user_id]
+			if user == nil
+				user = User.new(user_id)
+				@id_to_user[user_id] = user
+			end
+			user
+		end
+
+		def get_or_create_movie(movie_id)
+			movie = @id_to_movie[movie_id]
+			if movie == nil
+				movie = Movie.new(movie_id)
+				@id_to_movie[movie_id] = movie
+			end
+			movie
+		end
+
 end
--- movie_data_test.rb
+++ movie_data_test.rb
@@ -5,25 +5,79 @@ class PopularityTester
 	#MovieData.new.load_data
 	SECONDS_IN_YEAR = 3600.0 * 24.0 * 365.25
 
-	def test_popularity(movie_id, before_years)
-		timestamp_num = Time.now.to_i - (before_years * SECONDS_IN_YEAR)
-		rating_obj = Rating.new(nil, nil, 0, timestamp_num)
-		movie_obj = Movie.new(movie_id)
-		movie_obj.add_rating(rating_obj)
+	def run_test_dummy_popularity
+		puts "", "-------------------------------"
+		puts "run_test_dummy_popularity"
+		puts "-------------------------------", ""
 
-		puts "movie_id = #{movie_obj.id}"
-		puts "before_years = #{before_years}, timestamp = #{rating_obj.timestamp}"
-		puts "popularity = #{movie_obj.popularity}", ""
+		test_dummy_popularity(1, 0)
+		test_dummy_popularity(2, 1)
+		test_dummy_popularity(3, 4)
+		test_dummy_popularity(4, 5)
+		test_dummy_popularity(5, 6)
+		test_dummy_popularity(6, 10)
+		test_dummy_popularity(7, 100)
 	end
 
-	def run
-		tester = PopularityTester.new
-		tester.test_popularity(1, 0)
-		tester.test_popularity(2, 1)
-		tester.test_popularity(3, 4)
-		tester.test_popularity(4, 5)
-		tester.test_popularity(5, 6)
-		tester.test_popularity(6, 10)
-		tester.test_popularity(7, 100)
+	def run_test_read_popularity
+		puts "", "-------------------------------"
+		puts "run_test_read_popularity"
+		puts "-------------------------------", ""
+
+		movie_data = MovieData.new
+		movie_data.load_data
+
+		test_read_popularity(movie_data, 242)
+		test_read_popularity(movie_data, 302)
+		test_read_popularity(movie_data, 377)
+		test_read_popularity(movie_data, 51)
+		test_read_popularity(movie_data, 346)
+		test_read_popularity(movie_data, 474)
+		test_read_popularity(movie_data, 265)
+		test_read_popularity(movie_data, 465)
+		test_read_popularity(movie_data, 451)
+		test_read_popularity(movie_data, 86)
 	end
+
+	def print_popularity_list
+		puts "", "-------------------------------"
+		puts "print_popularity_list"
+		puts "-------------------------------", ""
+
+		movie_data = MovieData.new
+		movie_data.load_data
+
+		list = movie_data.popularity_list
+		0.upto(9) {|index| puts "#{index + 1}.: #{list[index].to_string}"}
+		puts "..."
+		
+		fromIndex, toIndex = list.length - 10, list.length - 1
+		fromIndex.upto(toIndex) {|index| puts "#{index + 1}.: #{list[index].to_string}"}
+	end
+
+	def run_all
+		#run_test_dummy_popularity
+		#run_test_read_popularity
+		print_popularity_list
+	end
+
+	private
+
+		def test_dummy_popularity(movie_id, before_years)
+			timestamp_num = Time.now.to_i - (before_years * SECONDS_IN_YEAR)
+			rating_obj = Rating.new(nil, nil, 0, timestamp_num)
+			movie_obj = Movie.new(movie_id)
+			movie_obj.add_rating(rating_obj)
+
+			puts "movie_id = #{movie_obj.id}"
+			puts "before_years = #{before_years}, timestamp = #{rating_obj.timestamp}"
+			puts "popularity = #{movie_obj.popularity}", ""
+		end
+
+		def test_read_popularity(movie_data, movie_id)
+			puts movie_data.movie(movie_id).to_string
+			puts "popularity = #{movie_data.popularity(movie_id)}", ""
+		end
 end
+
+PopularityTester.new.run_all
